ac8b5eb0127d2c5c92f31bd44c49e21d5337d90f
Andrew Harper <druharper@gmail.com>
2018-06-29 15:22:56 -0500

    Social Icons done but not appearing in Build

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="socialIconsBox">
-    <a class="socialIcon facebook" href="http://www.facebook.com/ValentinaRamosArt" target="_blank">
+    <a href="http://www.facebook.com/ValentinaRamosArt" target="_blank">
+      <div class="socialIcon facebook">&nbsp;</div>
     </a>
-    <a class="socialIcon instagram" href="https://www.instagram.com/valentinadesign/" target="_blank">
+    <a href="https://www.instagram.com/valentinadesign/" target="_blank">
+      <div class="socialIcon instagram">&nbsp;</div>
     </a>
-    <a class="socialIcon flickr" href="http://www.flickr.com/photos/valentinadesign/" target="_blank">
+    <a href="http://www.flickr.com/photos/valentinadesign/" target="_blank">
+      <div class="socialIcon flickr">&nbsp;</div>
     </a>
-    <a class="socialIcon pinterest" href="http://pinterest.com/valentinaharper/" target="_blank">
+    <a href="http://pinterest.com/valentinaharper/" target="_blank">
+      <div class="socialIcon pinterest">&nbsp;</div>
     </a>
   </div>
 </template>
@@ -22,28 +26,38 @@
     display: flex;
     flex-direction: column;
     flex-wrap: nowrap;
-    justify-content: flex-end;
+    justify-content: flex-start;
     align-items: flex-start;
-    align-content: flex-start;
+    align-content: stretch;
     float: right;
+    height: 250px;
+    width: 40px;
+    @include clearfix;
+
+    a {
+      border-bottom: none !important;
+      width: 40px !important;
+      height: 40px !important;
+      margin-bottom: .5em !important;
+      @include clearfix;
+    }
   }
 
-  a.socialIcon {
+  .socialIcon {
     width: 40px !important;
-    height: 40px;
-    background-size: contain;
-    background-repeat: no-repeat;
+    height: 40px !important;
+    background-size: contain !important;
+    background-repeat: no-repeat !important;
     // padding: 0; /* reset */
-    margin-bottom: .5em;
+    margin-bottom: .5em !important;
     display: block !important;
-    border-bottom: none;
     // flex-basis: auto;
-    // flex-grow: 0;
-    //
+    // flex-grow: 1;
+    @include clearfix;
 
     &.facebook {
       background-image: url(/static/img/icons/facebook.svg);
-      // display: block !important;
+      display: block !important;
 
       &:hover {
         background-image: url(/static/img/icons/facebook_hv.svg);
@@ -52,7 +66,7 @@
 
     &.instagram {
       background-image: url(/static/img/icons/instagram.svg);
-      // display: block !important;
+      display: block !important;
 
       &:hover {
         background-image: url(/static/img/icons/instagram_hv.svg);
@@ -61,7 +75,7 @@
 
     &.flickr {
       background-image: url(/static/img/icons/flickr.svg);
-      // display: block !important;
+      display: block !important;
 
       &:hover {
         background-image: url(/static/img/icons/flickr_hv.svg);
@@ -70,7 +84,7 @@
 
     &.pinterest {
       background-image: url(/static/img/icons/pinterest.svg);
-      // display: block !important;
+      display: block !important;
 
       &:hover {
         background-image: url(/static/img/icons/pinterest_hv.svg);
